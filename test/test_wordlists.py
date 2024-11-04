56891af2a444b5ad18e3b0b17db7ef9a628feb11
Martin Hosken <martin_hosken@sil.org>
2024-11-04 14:12:54 +0700

    Tidy up normaliser for modern khmer

--- test/test_wordlists.py
+++ test/test_wordlists.py
@@ -4,27 +4,35 @@ import os
 from difflib import Differ
 
 def test_normal(projectsdir, wordlist):
+    ''' Tests that the normalized word conforms to the regexp'''
     khnormal = os.path.join(os.path.dirname(__file__), '..', 'python', 'scripts', 'khnormal')
     res = subprocess.check_output([khnormal, "-n", "-f", f"{projectsdir}/{wordlist}"]).decode("utf-8")
     if len(res):
         print(res)
-        if wordlist == "cmo_words.txt":
+        if wordlist in ("bad_kh.txt", "cmo_words.txt", "cnd_words.txt"):
             pytest.xfail()
         else:
             pytest.fail()
 
-def test_input(projectsdir, wordlist):
+def test_input(projectsdir, wordlist, updatedata):
+    ''' Tests the input sequence to see if it is bad, before normalizing '''
     khnormal = os.path.join(os.path.dirname(__file__), '..', 'python', 'scripts', 'khnormal')
     res = subprocess.check_output([khnormal, "-N", "-n", "-f", f"{projectsdir}/{wordlist}"]).decode("utf-8")
-    if len(res):
-        failfile = os.path.join(os.path.dirname(__file__), 'failures', wordlist)
-        if not os.path.exists(failfile):
-            faildat = ""
-        else:
-            with open(failfile, encoding="utf-8") as inf:
-                faildat = inf.read()
-        if res != faildat:
-            d = Differ()
-            print("".join(d.compare(res, faildat)))
-            pytest.fail()
+    if not len(res):
+        return
+    lines = [s.strip() for s in res.split("\n") if len(s.strip())]
+    failfile = os.path.join(os.path.dirname(__file__), 'failures', wordlist)
+    if updatedata:
+        with open(failfile, "w", encoding="utf-8") as outf:
+            outf.write("\n".join(lines))
+        return
+    if not os.path.exists(failfile):
+        faildat = ""
+    else:
+        with open(failfile, encoding="utf-8") as inf:
+            faillines = [s.strip() for s in inf.readlines()]
+    if lines != faillines:
+        d = Differ()
+        print("\n".join(d.compare(lines, faillines)))
+        pytest.fail()
 
